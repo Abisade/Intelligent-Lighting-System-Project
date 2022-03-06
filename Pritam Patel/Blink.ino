3675800a017ee408decda7c9406a09b69ca6222b
Pritam Patel <31070175+pritampatel11@users.noreply.github.com>
2022-03-06 22:29:21 +0100

    Add files via upload

---
+++ Pritam Patel/Blink.ino
@@ -0,0 +1,27 @@
+
+byte recievedByte = 0;
+void setup() {
+
+// initialize digital pin D4 as an output
+Serial.begin(230400);
+pinMode(D4, OUTPUT);
+}
+
+// Loop finction to run repeatedly:
+void loop() {
+  
+  if(Serial.available()> 0)            // Is there any character coming through the serial port?
+  { 
+    recievedByte = Serial.read();      // If there is a character, read it.
+    
+    if(recievedByte=='M'){
+      digitalWrite(D4, HIGH);         // turn off LED
+      Serial.println("LED is switched OFF");
+    }    
+    else if(recievedByte=='P'){
+      digitalWrite(D4, LOW);          // turn on LED
+      Serial.println("LED is switched ON");
+      
+    }
+  }
+}

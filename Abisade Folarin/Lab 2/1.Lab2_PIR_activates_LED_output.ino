32a9b3f3ffe4212c1e51eda929a7eb4fb5d4f4c3
Bisade09 <65735189+Abisade@users.noreply.github.com>
2022-03-12 23:21:13 +0100

    Update and rename Lab2_PIR_with_LED_output.ino to 1.Lab2_PIR_activates_LED_output.ino

--- Abisade Folarin/Lab 2/1.Lab2_PIR_activates_LED_output.ino
+++ Abisade Folarin/Lab 2/1.Lab2_PIR_activates_LED_output.ino
@@ -1,4 +1,4 @@
-//The PIR reads motion and then activates the LED to go ON or OFF accordingly
+//The Passive Infrared (PIR) sensor (HC SR501) detects motion and then activates the seven-colour blink LED to go either ON or OFF accordingly.
 
 int ledPin = LED_BUILTIN;          //Pin for the LED
 int inputPin = D1;                 //data pin for PIR sensor
@@ -6,9 +6,9 @@ int pirState = LOW;                //initially assume no moiton detected
 int val = 0;                       //variable for reading pin status
 
 void setup() {
-  pinMode(ledPin, OUTPUT);        //Declare LED as output
-  pinMode(inputPin, INPUT);       //Declare the sensor data pin as input
-  pinMode(D2, OUTPUT);
+  pinMode(ledPin, OUTPUT);        //Declare ESP8266 LED as output
+  pinMode(inputPin, INPUT);       //Declare the PIR sensor data pin D1 as input
+  pinMode(D2, OUTPUT);            //Declare the LED light D2 as output
 
   Serial.begin(9600);
 }
@@ -16,15 +16,15 @@ void setup() {
 void loop() {
   val = digitalRead(inputPin);      //read input value 
   if (val == HIGH) {                //check if the input is HIGH  
-    digitalWrite(ledPin, LOW);      //turn board LED ON
-    digitalWrite(D2, HIGH);         //turn main LED ON
+    digitalWrite(ledPin, LOW);      //turn ON board ESP8266
+    digitalWrite(D2, HIGH);         //turn ON LED light D2
     if (pirState == HIGH) {
       Serial.println("Motion is Detected!");
       pirState = LOW;
     }
   } else {
-    digitalWrite(ledPin, HIGH);    // turn board LED OFF
-    digitalWrite(D2, LOW);         //turn main LED OFF
+    digitalWrite(ledPin, HIGH);    // turn OFF board ESP8266
+    digitalWrite(D2, LOW);         //turn OFF LED light D2
     if (pirState == LOW) {
       Serial.println("Motion has ended!");
       pirState = HIGH;

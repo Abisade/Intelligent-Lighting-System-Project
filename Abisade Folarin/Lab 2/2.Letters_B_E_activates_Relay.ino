e4af91d0e0e60f1f6ce6b51566dadcd8b2151b8a
Bisade09 <65735189+Abisade@users.noreply.github.com>
2022-03-12 23:31:07 +0100

    Update and rename Lab2_Relay_Blink_Code.ino to 2.Letters_B_E_activates_Relay.ino

--- Abisade Folarin/Lab 2/2.Letters_B_E_activates_Relay.ino
+++ Abisade Folarin/Lab 2/2.Letters_B_E_activates_Relay.ino
@@ -1,4 +1,6 @@
-//Blink code by Bisade
+//Program allows for controlling the Relay and embedded LED through the Serial Monitor. 
+//Thus, when the ESP8266 receives my “ON character - B” the embedded LED and Relay will be switched on, and, 
+//when sending my “OFF character - E”, the embedded LED and Relay will be switched off.
 
 int relayPin = D1;
 byte recievedByte = 0;
@@ -18,13 +20,13 @@ void loop() {
     recievedByte = Serial.read();      // If there is a character, read it.
     
     if(recievedByte=='E'){
-      digitalWrite(D4, HIGH);           // turn OFF ESP8266 LED
+      digitalWrite(D4, HIGH);           // turn OFF embedded LED
       digitalWrite(relayPin, LOW);      //turn OFF relay LED
       Serial.println("LED is switched OFF");
       //delay(3000);                    //if activated, will delay for 3 secs
     }    
     else if(recievedByte=='B'){
-      digitalWrite(D4, LOW);            // turn ON ESP8266 LED
+      digitalWrite(D4, LOW);            // turn ON embedded LED
       digitalWrite(relayPin, HIGH);     //turn ON relay LED
       Serial.println("LED is switched ON");
       //delay(2000);                    //if activated, will delay for another 2 secs

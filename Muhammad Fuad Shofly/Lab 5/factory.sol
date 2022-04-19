17b8290976ecb4d6fe824726eab1dec751a181d6
shofly99 <100284932+shofly99@users.noreply.github.com>
2022-04-19 18:05:39 +0200

    Add files via upload

---
+++ Muhammad Fuad Shofly/Lab 5/factory.sol
@@ -0,0 +1,60 @@
+//SPDX-License-Identifier: GLP-3.0
+pragma solidity >=0.5.0 <0.8.0;
+
+contract Factory { 
+    uint idDigits=16;
+    uint idModulus=10^idDigits;
+
+    struct Product {
+        string name;
+        uint id;
+    }
+    Product[] public products;
+
+    event NewProduct (uint ArrayProductId,  string name, uint id);
+
+    function _createProduct(string memory _name, uint _id) private {
+        products.push(Product(_name,_id));
+        uint productId= (products.length - 1);
+        emit NewProduct(productId,_name,_id); 
+    }
+    function _generateRandomId(string memory _str) private view returns(uint) {
+        uint rand=uint(keccak256(abi.encodePacked(_str)));
+        return (rand%idModulus);
+    } 
+    function createRandomProduct(string memory _name) public {
+        uint randId=_generateRandomId(_name);
+        _createProduct(_name, randId);
+    } 
+
+    mapping (uint => address) public productToOwner;
+    mapping (address => uint) ownerProductCount;
+
+    function _Ownership (uint _productId) private { 
+        productToOwner[_productId] = msg.sender;
+        ownerProductCount[msg.sender]++;
+    }
+
+    function getProductsByOwner (address _owner) external view returns(uint[] memory) { 
+        uint counter=0;
+        uint[] memory result= new uint[](ownerProductCount[_owner]);
+        for (uint i=0; i<products.length; i++) {
+            if (productToOwner[i]==_owner) {
+                result[counter]=i;
+                counter++;
+            }
+        }
+        return result;              
+    }
+
+    mapping (uint => uint) public age; 
+
+    modifier olderThan (uint _age, uint _userId) {
+        require (age[_userId] >= _age, "You Must Be At Least 18 Years Old");
+        _;
+    }
+
+    function validationUsers (uint _userId) public olderThan(18, _userId) {
+    }
+
+}
